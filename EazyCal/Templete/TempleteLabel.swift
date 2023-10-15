c4b7abe20ad3e7338603a571478f570f38c0b3ec
HansolWorld <hs9269@gmail.com>
2023-10-15 14:19:35 +0900

    Add templeteLabel

--- EazyCal/Templete/TempleteLabel.swift
+++ EazyCal/Templete/TempleteLabel.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct TempleteLabel: View {
+    let name: String
+    let color: UIColor
+    let icon: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 8) {
+            ZStack {
+                Circle()
+                    .frame(width: 44, height: 44)
+                    .opacity(0.1)
+                Text(icon)
+                    .font(.icon)
+            }
+            Text(name)
+                .font(.templete)
+        }
     }
 }
 
 #Preview {
-    TempleteLabel()
+    TempleteLabel(name: "회의록", color: .blue, icon: "😀")
 }
