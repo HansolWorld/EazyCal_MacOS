83195688a966f3e64c96527bfe474b17b6ef508f
HansolWorld <hs9269@gmail.com>
2023-10-15 14:20:14 +0900

    Rename contentview

--- EazyCal/MainView.swift
+++ EazyCal/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  EazyCal
 //
 //  Created by apple on 10/13/23.
@@ -8,7 +8,7 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+struct MainView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
@@ -82,5 +82,5 @@ private let itemFormatter: DateFormatter = {
 }()
 
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    MainView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
