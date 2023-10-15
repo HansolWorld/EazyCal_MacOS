b6eab00c55c4648174a2c8f628b22cc9c9212b66
HansolWorld <hs9269@gmail.com>
2023-10-15 14:19:53 +0900

    Add todoLabel

--- EazyCal/Todo/TodoLabel.swift
+++ EazyCal/Todo/TodoLabel.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct TodoLabel: View {
+    let task: String
+    let date: Date
+    let importance: Importance
+    @Binding var isCheck: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 8) {
+            Button(action: {
+                isCheck.toggle()
+            }) {
+                Image(systemName: checkToImageName())
+            }
+            Text(task)
+                .font(.category)
+            Spacer()
+            Text(caclulatorDay())
+                .font(.dday)
+        }
+        .tint(.black)
+        .padding()
+    }
+    
+    func checkToImageName() -> String {
+        switch self.isCheck {
+        case true:
+            return SFSymbol.checkCircle.name
+        case false:
+            return SFSymbol.circle.name
+        }
+    }
+
+    func caclulatorDay() -> String{
+        let currentDay = Date()
+        
+        let dday = Int(Date().timeIntervalSince(date)/86400)
+        
+        if dday < 0 {
+            return "\(dday)일 지남"
+        } else if dday == 0 {
+            return "오늘"
+        } else {
+            return "\(dday)일 전"
+        }
     }
 }
 
 #Preview {
-    TodoLabel()
+    TodoLabel(task: "중간점검", date: Date(), importance: .high, isCheck: .constant(true))
 }
