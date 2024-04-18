2f23d2c1396756b21ad3658d8094324c6d63d143
HansolWorld <hs9269@gmail.com>
2024-04-19 00:27:50 +0900

    feat: 사이드탭 접힘 기능 추가

--- EazyCal/Home.swift
+++ EazyCal/Home.swift
@@ -10,33 +10,52 @@ import SwiftUI
 struct Home: View {
     @StateObject var homeData = HomeViewModel()
     @State var currentDragTemplate: Template?
+    @State var isSide = true
     var body: some View {
-        HStack(spacing: 0) {
-            HStack(spacing:0) {
-                VStack(spacing: 24) {
-                    TabButton(image: "calendar", title: "Calendar", selectedTab: $homeData.selectedTab)
-                    TabButton(image: "list.bullet", title: "List", selectedTab: $homeData.selectedTab)
-                    TabButton(image: "doc.plaintext", title: "ScheduleList", selectedTab: $homeData.selectedTab)
-                    Spacer()
-                }
-                .padding()
-                .padding(.top, 30)
-                
-                ZStack {
-                    switch homeData.selectedTab {
-                    case "Calendar":
-                        CalendarTabView(currentDragTemplate: $currentDragTemplate)
-                    case "List":
-                        TodoView()
-                    default:
-                        ScheduleListView()
+        ZStack(alignment: .topLeading) {
+            HStack(spacing: 0) {
+                    HStack(spacing:0) {
+                        VStack(spacing: 24) {
+                            TabButton(image: "calendar", title: "Calendar", selectedTab: $homeData.selectedTab)
+                            TabButton(image: "list.bullet", title: "List", selectedTab: $homeData.selectedTab)
+                            TabButton(image: "doc.plaintext", title: "ScheduleList", selectedTab: $homeData.selectedTab)
+                            Spacer()
+                        }
+                        .padding()
+                        .padding(.top, 30)
+                        
+                        if isSide {
+                            ZStack {
+                                switch homeData.selectedTab {
+                                case "Calendar":
+                                    CalendarTabView(currentDragTemplate: $currentDragTemplate)
+                                case "List":
+                                    TodoView()
+                                default:
+                                    ScheduleListView()
+                                }
+                            }
+                            .background(Color.white)
+                            .frame(maxWidth: 330-52)
+                        }ㅎ
                     }
-                }
-                .background(Color.white)
+                
+                CalenderView(currentDragTemplate: $currentDragTemplate)
             }
-            .frame(maxWidth: 330)
             
-            CalenderView(currentDragTemplate: $currentDragTemplate)
+            Button(action: {
+                withAnimation {
+                    isSide.toggle()
+                }
+            }) {
+                Image(systemName: "sidebar.left")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 14)
+            }
+            .buttonStyle(.plain)
+            .padding(.leading, 70)
+            .padding(.top, 8)
         }
         .background(Color.background)
         .environmentObject(homeData)
